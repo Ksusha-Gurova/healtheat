f2c5620930ceb346e7bda8436319a0a6ff310ca3
kgurova <kgurova@neoflex.ru>
2024-02-11 17:25:57 +0300

    init

---
+++ src/main/java/ru/kgurova/healtheat/HealtheatApplication.java
@@ -0,0 +1,13 @@
+package ru.kgurova.healtheat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealtheatApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HealtheatApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ru/kgurova/healtheat/HealtheatApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kgurova.healtheat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealtheatApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
